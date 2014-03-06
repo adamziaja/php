52d8947b370e09d3872fd00762fa10a00a8e064e
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-03-06 11:16:42 +0100

    example of use URLQuery API v3

---
+++ urlquery_api_v3.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php
+
+/*
+ * example of use URLQuery API v3
+ * (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
+ */
+
+$context = array(
+    'http' => array(
+        'header' => 'Content-type: application/x-www-form-urlencoded',
+        'method' => 'POST',
+        'content' => '{"method": "report_list", "key": "_YOUR-API-KEY_"}', // change me
+    ),
+);
+$file = file_get_contents('https://_API-URL_/v3/json', false, stream_context_create($context)); // change me ("Please do not share this URL unless needed.")
+$array = json_decode($file, true);
+
+if ($array['_status_']['status'] == 'ok') {
+    foreach ($array['reports'] as $report) {
+        if ($report['url']['ip']['cc'] == 'PL') { // change me
+            //echo $report['report_id'] . "\n";
+            var_dump($report);
+        }
+    }
+}
+?>
