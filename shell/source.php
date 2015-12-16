24305df4512dadb7bf1a29da6beacb22a8060aa7
Adam Ziaja <adamziaja@users.noreply.github.com>
2015-12-16 19:32:17 +0100

    Update source.php

--- shell/source.php
+++ shell/source.php
@@ -1,5 +1,69 @@
-<?php
-if(isset($_GET['a'])){
-    echo highlight_string(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/'.$_GET['a'])) . PHP_EOL;
-}
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <style type="text/css">
+            body {
+                font-size: 12px;
+                font-family: Verdana;
+            }
+            a:link,
+            a:visited,
+            a:hover,
+            a:active { 
+                color: blue;
+                font-weight: normal;
+            }
+            .dir {
+                background-color: #99CCFF !important;
+            }
+            .file {
+                background-color: #FFCC66 !important;
+            }
+        </style>
+    </head> 
+    <body><?php
+        // (C) 2015 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
+
+        $back = '<p><a href="javascript:history.back()">&laquo; back</a></p>' . PHP_EOL;
+        echo $back;
+        if (isset($_GET['s'])) {
+            highlight_string(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/' . $_GET['s'])) . PHP_EOL;
+        } else {
+            if (isset($_GET['d'])) {
+                $dir = $_GET['d'];
+            } else {
+                $dir = NULL;
+            }
+
+            $filestack = array();
+            $dirstack = array();
+
+            foreach (new DirectoryIterator('./' . $dir) as $file) {
+                if (!$file->isDot()) {
+                    if ($file->isFile()) {
+                        array_push($filestack, $file->getFilename());
+                    } elseif ($file->isDir()) {
+                        array_push($dirstack, $file->getFilename());
+                    }
+                }
+            }
+
+            sort($filestack);
+            sort($dirstack);
+
+            foreach ($dirstack as $file) {
+                echo '<a class="dir" href="?d=' . $dir . '/' . $file . '">' . $file . '</a><br>' . PHP_EOL;
+            }
+
+            foreach ($filestack as $file) {
+                if (strpos($file, '.php')) {
+                    echo '<a class="file" href="?s=' . $dir . '/' . $file . '">' . $file . '</a><br>' . PHP_EOL;
+                } else {
+                    echo '<a href="?s=' . $dir . '/' . $file . '">' . $file . '</a><br>' . PHP_EOL;
+                }
+            }
+        }
+        echo $back;
+        ?>
+    </body>
+</html>
