b6aab2dbc8c86ba05b710fd5298c4c43ff402667
Adam Ziaja <adamziaja@users.noreply.github.com>
2017-02-27 13:15:33 +0100

    Create forwarder.php

---
+++ forwarder.php
@@ -0,0 +1,22 @@
+<?php
+/*
+(C) 2017 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
+
+1. visit url
+2. grab all links
+3. check for first link that have regexp value
+4. forward to a:href url
+*/
+ini_set('user_agent', 'Opera/9.80 (Windows NT 6.1; U; Edition Campaign 21; en-GB) Presto/2.7.62 Version/11.00');
+$str = file_get_contents('http://***URL***');
+$DOM = new DOMDocument;
+@$DOM->loadHTML($str);
+$items = $DOM->getElementsByTagName('a');
+for ($i = 0; $i < $items->length; $i++) {
+    if (preg_match('/***A-VALUE***/i', $items->item($i)->nodeValue)) {
+        $url = $items->item($i)->getAttribute('href');
+        header("Location: $url", true, 302);
+        echo "<a href='$url'>" . $url . '</a>' . PHP_EOL;
+    }
+}
+?>
